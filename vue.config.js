a0445c752145534f7ba9e67b7a4eed579a11c19b
jeonga-Son <jjeong0106@gmail.com>
2023-03-24 17:47:28 +0900

    feat : connect with springboot

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
 })
+
+module.exports = {
+  devServer: {
+    proxy : 'http://localhost:8081'
+  }
+}
